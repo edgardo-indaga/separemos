610b41805683ef2aa16cca36044b0f14683ac3c4
edgardoruotolo <edgardoruotolo@crowadvance.com>
2025-03-18 13:17:03 -0300

    Task: Link PDF Date: 18-03-2025 Version: 2.2

--- src/components/DownloadMagazine/DownloadMagazine.tsx
+++ src/components/DownloadMagazine/DownloadMagazine.tsx
@@ -1,35 +1,15 @@
 'use client';
 
 export default function DownloadMagazine() {
-    const handleDownloadPDF = () => {
-        // Ruta del PDF en la carpeta public
-        const pdfPath = '/nombre-del-archivo.pdf';
-
-        // Crear un enlace temporal
-        const link = document.createElement('a');
-        link.href = pdfPath;
-
-        // Establecer el nombre de archivo para la descarga
-        link.download = 'revista-separemos-las-aguas.pdf';
-
-        // Agregar el enlace al body
-        document.body.appendChild(link);
-
-        // Disparar el clic del enlace
-        link.click();
-
-        // Remover el enlace del body
-        document.body.removeChild(link);
-    };
-
     return (
         <>
-            <button
+            <a
                 className="bg-yellow font-barlow text-skyblue hover:bg-yellow/90 cursor-pointer rounded-[30px] px-[32px] py-[8px] text-[20px] font-bold uppercase"
-                onClick={handleDownloadPDF}
+                href="https://issuu.com/julietaindaga/docs/separemos_las_aguas"
+                target="_blank"
             >
                 Descargar
-            </button>
+            </a>
         </>
     );
 }
