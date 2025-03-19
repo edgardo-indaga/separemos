d0d5a6bfcf123b337aa404f381c0cf4f564b997c
edgardoruotolo <edgardoruotolo@crowadvance.com>
2025-03-18 21:53:50 -0300

    Task: Meta Primera Versión Date: 18-03-2025 Version: 2.6

---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://www.separemoslasaguas.cl/',
+            lastModified: new Date('2025-03-18'),
+            changeFrequency: 'daily',
+            priority: 1,
+        },
+    ];
+}
--- src/components/RedesSociales/RedesSociales.tsx
+++ src/components/RedesSociales/RedesSociales.tsx
@@ -1,10 +1,22 @@
+'use client';
+
 import Image from 'next/image';
 
+import {
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+} from 'react-share';
+
 export default function RedesSociales() {
-    const instagram: string = '/icon-instagram.svg';
     const facebook: string = '/icon-facebook.svg';
-    const youtube: string = '/icon-youtube.svg';
     const x: string = '/icon-x.svg';
+    const whatsapp: string = '/icon-whatsapp.svg';
+    const linkedin: string = '/icon-linkedin.svg';
+
+    const shareUrl = 'https://www.separemoslasaguas.cl/';
+    const title = 'Separemos las aguas';
 
     return (
         <>
@@ -14,18 +26,7 @@ export default function RedesSociales() {
                 </h6>
                 <ul className="flex flex-row">
                     <li className="px-[4px] py-[2px]">
-                        <a href="https://www.instagram.com/suraliscontigo/" target="_blank">
-                            <Image
-                                src={instagram}
-                                alt="Instagram"
-                                width={64}
-                                height={64}
-                                className="h-[50px] w-[50px] md:h-[38px] md:w-[38px]"
-                            />
-                        </a>
-                    </li>
-                    <li className="px-[4px] py-[2px]">
-                        <a href="https://www.facebook.com/SuralisContigo/" target="_blank">
+                        <FacebookShareButton url={shareUrl} title={title}>
                             <Image
                                 src={facebook}
                                 alt="Facebook"
@@ -33,21 +34,10 @@ export default function RedesSociales() {
                                 height={64}
                                 className="h-[50px] w-[50px] md:h-[38px] md:w-[38px]"
                             />
-                        </a>
+                        </FacebookShareButton>
                     </li>
                     <li className="px-[4px] py-[2px]">
-                        <a href="https://www.youtube.com/@Separemoslasaguas" target="_blank">
-                            <Image
-                                src={youtube}
-                                alt="Youtube"
-                                width={64}
-                                height={64}
-                                className="h-[50px] w-[50px] md:h-[38px] md:w-[38px]"
-                            />
-                        </a>
-                    </li>
-                    <li className="px-[4px] py-[2px]">
-                        <a href="https://x.com/SuralisTeAyuda" target="_blank">
+                        <TwitterShareButton url={shareUrl} title={title}>
                             <Image
                                 src={x}
                                 alt="X"
@@ -55,7 +45,31 @@ export default function RedesSociales() {
                                 height={64}
                                 className="h-[50px] w-[50px] md:h-[38px] md:w-[38px]"
                             />
-                        </a>
+                        </TwitterShareButton>
+                    </li>
+
+                    <li className="px-[4px] py-[2px]">
+                        <WhatsappShareButton url={shareUrl} title={title}>
+                            <Image
+                                src={whatsapp}
+                                alt="Whatsapp"
+                                width={64}
+                                height={64}
+                                className="h-[50px] w-[50px] md:h-[38px] md:w-[38px]"
+                            />
+                        </WhatsappShareButton>
+                    </li>
+
+                    <li className="px-[4px] py-[2px]">
+                        <LinkedinShareButton url={shareUrl} title={title}>
+                            <Image
+                                src={linkedin}
+                                alt="Whatsapp"
+                                width={64}
+                                height={64}
+                                className="h-[50px] w-[50px] md:h-[38px] md:w-[38px]"
+                            />
+                        </LinkedinShareButton>
                     </li>
                 </ul>
             </div>
