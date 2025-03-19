cea82ca834774bbe2e8657599fc907cc421ad995
edgardoruotolo <edgardoruotolo@crowadvance.com>
2025-03-18 21:59:32 -0300

    Task: Archivo PDF Date: 18-03-2025 Version: 2.7

--- src/components/DownloadMagazine/DownloadMagazine.tsx
+++ src/components/DownloadMagazine/DownloadMagazine.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 export default function DownloadMagazine() {
+    const pdfUrl: string =
+        'https://rmp0hr1dqiabvbul.public.blob.vercel-storage.com/archive/revista-0mPLCJdlyrm7u9pvkwiaPyKvQxqDhc.pdf';
+
     return (
         <>
             <a
                 className="bg-yellow font-barlow text-skyblue hover:bg-yellow/90 cursor-pointer rounded-[30px] px-[32px] py-[8px] text-[20px] font-bold uppercase"
-                href="https://issuu.com/julietaindaga/docs/separemos_las_aguas"
+                href={pdfUrl}
                 target="_blank"
+                rel="noopener noreferrer"
+                download
             >
                 Descargar
             </a>
