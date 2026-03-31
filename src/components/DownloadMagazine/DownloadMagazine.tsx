6ebb6fe198c89f5cf2a095194590cb624a45812b
Edgardo Ruotolo <edgardoruotolo@gmail.com>
2026-03-31 12:22:35 -0300

    Task: PDF Date: 31-03-2026 Version: 3.2

--- src/components/DownloadMagazine/DownloadMagazine.tsx
+++ src/components/DownloadMagazine/DownloadMagazine.tsx
@@ -2,12 +2,12 @@
 
 export default function DownloadMagazine() {
     const pdfUrl: string =
-        'https://rmp0hr1dqiabvbul.public.blob.vercel-storage.com/archive/revista-0mPLCJdlyrm7u9pvkwiaPyKvQxqDhc.pdf';
+        '/revista.pdf';
 
     return (
         <>
             <a
-                className="bg-yellow font-barlow text-skyblue hover:bg-yellow/90 cursor-pointer rounded-[30px] px-[32px] py-[8px] text-[20px] font-bold uppercase"
+                className="bg-yellow font-barlow text-skyblue hover:bg-yellow/90 cursor-pointer rounded-[30px] px-8 py-2 text-[20px] font-bold uppercase"
                 href={pdfUrl}
                 target="_blank"
                 rel="noopener noreferrer"
